aa61d00c167c75030dc00d69940f26af155383f9
Maret Idris <maret@fasterlap.com>
2018-07-16 16:19:42 -0700

    Initial commit

---
+++ jscode.js
@@ -0,0 +1,21 @@
+//
+// Media Query for intro text
+//
+function handleIntroText(mediaQuery) {
+    if (document.getElementById('animate-text') != null) {
+        // If media query matches
+        if (mediaQuery.matches) {
+            document.getElementById('animate-text').innerHTML =
+                "We are a design and<br>software development<br>consulting company<br>based in<br>Silicon Valley";
+        } else {
+            document.getElementById('animate-text').innerHTML =
+                "We are a design and software<br>development consulting company<br>based in Silicon Valley";
+        }
+    }
+}
+
+const mediaQuery400px = window.matchMedia("(max-width: 400px)");
+// Call listener function at run time
+handleIntroText(mediaQuery400px);
+// Attach listener function on state changes
+mediaQuery400px.addListener(handleIntroText);
